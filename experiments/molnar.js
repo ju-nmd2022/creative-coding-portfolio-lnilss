a490d5cae3bc4716aa784b639d83306105037e60
Linnea <nili22re@student.ju.se>
2024-09-02 13:47:33 +0200

    molnar colours + blend modes
    artistic take on Vera Molnar

--- experiments/molnar.js
+++ experiments/molnar.js
@@ -1,30 +1,25 @@
-//grid
-//decreased square with slight variation
-//around 10 steps
-//no gap
-
 //Molnar recreation
 
 //values
-const size = 100; //what should size be? parameters are good for reusing code
+const size = 100; 
 const layers = 10;
 
 function getRandomValue(pos, variance) {
     return pos + map(Math.random(), 0, 1, -variance, variance); //maps from one system to a different one
 }
 function drawLayers(x, y, size, layers) {
-    //to not calculate over and over
 
-    const variance = size / 20;
+    const variance = size / 60;
 
     noFill();
+    stroke(random(360), 100, 100);
     // rectMode(CENTER); we will not use this and instead use own calculations
     for (let i = 0; i < layers; i++) {
-        if (Math.random() > 0.8) {
+        if (Math.random() > 0.3) {
           continue;
         }
         const s = (size / layers) * i;
-        const half = s / 2;
+        const half = s / 6;
         beginShape();
         vertex(
           getRandomValue(x - half, variance),
@@ -49,10 +44,14 @@ function drawLayers(x, y, size, layers) {
 
 function setup() {
     createCanvas(1000,1000);
+    frameRate(2);
+
+    colorMode(HSB, 360, 100, 100);
 }
 
 function draw() {
-    background(255,255,255);
+    background(200, 0, 120);
+    blendMode(MULTIPLY);
 
     // drawLayers(100, 100, size, layers);
     for (let y = 0; y < 10; y++) {
@@ -60,6 +59,5 @@ function draw() {
           drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
         }
       }
-
-    noLoop();
+      blendMode(BLEND);
 }
